807c269092338c33ef303f02323a9e2f5e0949d8
Ben Cooke <30628036+bentcooke@users.noreply.github.com>
2018-10-01 16:29:54 -0500

    Update linux-config.cmake

--- linux-config.cmake
+++ linux-config.cmake
@@ -3,7 +3,8 @@ include_directories(${CMAKE_CURRENT_SOURCE_DIR}/../)
 
 set(SA_PV_OS_NAME "linux")
 
-SET(PAL_TLS_BSP_DIR ${NEW_CMAKE_SOURCE_DIR}/mbed-cloud-client/mbed-client-pal/Configs/mbedTLS)
+#SET(PAL_TLS_BSP_DIR ${NEW_CMAKE_SOURCE_DIR}/mbed-cloud-client/mbed-client-pal/Configs/mbedTLS)
+SET(PAL_TLS_BSP_DIR ${NEW_CMAKE_SOURCE_DIR}/mbed-cloud-client/mbed-client-pal/Configs/${TLS_LIBRARY})
 
 add_definitions(
 	-DMBED_CONF_MBED_TRACE_ENABLE=1
@@ -11,4 +12,6 @@ add_definitions(
 	-DPAL_USER_DEFINED_CONFIGURATION="\\"source/sotp_fs_config_linux.h"\\"
 	-DMBED_CLIENT_USER_CONFIG_FILE="\\"source/mbed_cloud_client_user_config.h"\\"
 	-DMBED_CLOUD_CLIENT_USER_CONFIG_FILE="\\"source/mbed_cloud_client_user_config.h"\\"
+	-DPAL_FS_MOUNT_POINT_PRIMARY="\\"/var/config/mbed\\""
+	-DPAL_FS_MOUNT_POINT_SECONDARY="\\"/var/config/mbed\\""
 )
